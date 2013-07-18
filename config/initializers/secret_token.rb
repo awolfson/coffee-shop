8c9e8b14b3ca2c14bb4fab2bf3524520c195e076
Aaron Wolfson <awolfson0@gmail.com>
2013-07-17 21:35:50 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoffeeShop::Application.config.secret_token = '3ff1fe54fbf9d8254c5e78bfe6e9da21ba305d2cb73053f1d55d4972aee7e753ce16cf55a608f47b4b8103f7a0acfb2cf3e7439c600acd22ec6c4b2477e971c8'
