05f27a4bbf8215e0ba1634cfdfb1dbdbc12cdfb1
Aaron Wolfson <awolfson0@gmail.com>
2013-09-02 14:54:28 -0500

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,10 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password, :password_confirmation
+  attr_accessible :email, :name, :username, :password, :password_confirmation
   has_secure_password
 
-  before_save { |user| user.email = email.downcase }
+  before_save { email.downcase! }
 
   validates :name, 	presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 CoffeeShop::Application.routes.draw do
 
-  get "users/new"
+  resources :users
 
   root to: 'static_pages#home'
   
